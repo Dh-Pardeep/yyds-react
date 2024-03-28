a1ca5f19c33dfca69f79efce55444d3e204ed105
dh-pardeep <pardeepjangra514@gmil.com>
2024-03-28 10:46:22 +0530

    setup

---
+++ src/components/view/OurBuidlers.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const OurBuidlers = () => {
+  return (
+    <div>OurBuidlers</div>
+  )
+}
+
+export default OurBuidlers
--- tailwind.config.js
+++ tailwind.config.js
@@ -1,11 +1,28 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: [
-    "./src/**/*.{js,jsx,ts,tsx}",
-  ],
+  content: ["./src/**/*.{js,jsx,ts,tsx}"],
   theme: {
-    extend: {},
+    extend: {
+      fontSize: {
+        "2xl": "28px",
+        "5xl": "50px",
+        "6xl": "64px",
+      },
+      fontFamily: {
+        poppins: ['"Poppins", sans-serif'],
+        indieFlower: ['"Indie Flower", cursive'],
+        montserrat_alternates: ['"Montserrat+Alternates", cursive'],
+      },
+      colors: {
+        lightBlack: "#010101",
+        saffron: "#F8c338",
+        yellowRed: "#F2BD64",
+        skyBlue: "#31ABDF",
+        papayaWhip: "#FFF0CE",
+        racingRed: "#D70101",
+        pastedOrange: "#F4B03B",
+      },
+    },
   },
   plugins: [],
-}
-
+};
